731ee6142fcdba09a71e937c4b116c195dde134d
Lor Hengrith <155905861+SthNoWork@users.noreply.github.com>
2026-01-04 11:15:42 +0700

    finalkly works but its slow asf

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/hospital?useSSL=true&serverTimezone=UTC";
+            "jdbc:mysql://192.168.0.147:3306/hospital?useSSL=true&serverTimezone=UTC";
 
     private static String username = "doctor";
     private static String password = "trustmeiamadoctor";
